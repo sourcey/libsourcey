9ce153b486f951c4ec8cc7a79a1679cf7345bd4a
Yury Shubin <crusher83@gmail.com>
2016-03-30 21:15:35 +0300

    scyconfig.h path fix

--- cmake/LibSourceyIncludes.cmake
+++ cmake/LibSourceyIncludes.cmake
@@ -215,6 +215,7 @@ macro(set_default_project_directories)
 
   include_directories(${CMAKE_CURRENT_SOURCE_DIR}/include)
   include_directories(${LibSourcey_INCLUDE_DIRS})
+  include_directories(${CMAKE_BINARY_DIR})  
   link_directories(${LibSourcey_LIBRARY_DIRS})
 endmacro()
 
