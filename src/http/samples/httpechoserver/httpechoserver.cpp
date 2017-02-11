30975ee31e71fa8979d59fea5c07120bab293cdf
Kam Low <auscaster@gmail.com>
2017-02-11 13:53:06 +0100

    Update HTTP echop server sample

--- src/http/samples/httpechoserver/httpechoserver.cpp
+++ src/http/samples/httpechoserver/httpechoserver.cpp
@@ -11,6 +11,7 @@ using namespace scy::net;
 
 
 const std::uint16_t HttpPort = 1337;
+const net::Address address("0.0.0.0", HttpPort);
 
 
 #if 0
@@ -46,22 +47,24 @@ void raiseLoadBalancedServer()
 
 void raiseEchoServer()
 {
-    http::Server srv(net::Address("0.0.0.0", HttpPort));
+    http::Server srv(address);
     srv.start();
 
     srv.Connection += [](http::ServerConnection::Ptr conn) {
         conn->Payload += [](http::ServerConnection& conn, const MutableBuffer& buffer) {
             conn.send(bufferCast<const char*>(buffer), buffer.size());
+            conn.close();
         };
     };
 
+    std::cout << "HTTP server listening on " << address << endl;
     uv::waitForShutdown();
 }
 
 
 void raiseBenchmarkServer()
 {
-    http::Server srv(net::Address("0.0.0.0", HttpPort));
+    http::Server srv(address);
     srv.start();
 
     srv.Connection += [&](http::ServerConnection::Ptr conn) {
@@ -69,6 +72,7 @@ void raiseBenchmarkServer()
         conn->close();
     };
 
+    std::cout << "HTTP server listening on " << address << endl;
     uv::waitForShutdown();
 }
 
@@ -80,27 +84,27 @@ int main(int argc, char** argv)
     //Logger::instance().setWriter(new AsyncLogWriter);
     net::SSLManager::initNoVerifyServer();
     {
-        //raiseServer();
-        raiseBenchmarkServer();
-        //libuv::raiseLibuvEchoServer();
-// #if SCY_HAS_KERNEL_SOCKET_LOAD_BALANCING
-//         Thread t1;
-//         Thread t2;
-//         // Thread t3;
-//         // Thread t4;
-//
-//         t1.start(std::bind(raiseLoadBalancedServer));
-//         t2.start(std::bind(raiseLoadBalancedServer));
-//         // t3.start(std::bind(raiseLoadBalancedServer));
-//         // t4.start(std::bind(raiseLoadBalancedServer));
-//
-//         t1.join();
-//         t2.join();
-//         // t3.join();
-//         // t4.join();
-// #else
-//         raiseServer();
-// #endif
+#if SCY_HAS_KERNEL_SOCKET_LOAD_BALANCING
+        Thread t1;
+        Thread t2;
+        // Thread t3;
+        // Thread t4;
+
+        t1.start(std::bind(raiseBenchmarkServer));
+        t2.start(std::bind(raiseBenchmarkServer));
+        // t3.start(std::bind(raiseLoadBalancedServer));
+        // t4.start(std::bind(raiseLoadBalancedServer));
+
+        t1.join();
+        t2.join();
+        // t3.join();
+        // t4.join();
+#else
+        raiseServer();
+#endif
+      // raiseServer();
+      // raiseBenchmarkServer();
+      // libuv::raiseLibuvEchoServer();
     }
     net::SSLManager::instance().shutdown();
     Logger::destroy();
