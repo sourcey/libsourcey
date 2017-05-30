ea03730283e59173de3153b5cdfe6c534876b188
Kam <auscaster@gmail.com>
2017-05-30 17:58:04 +0200

    Fix log level definition

--- src/webrtc/samples/webrtcstreamer/main.cpp
+++ src/webrtc/samples/webrtcstreamer/main.cpp
@@ -37,7 +37,7 @@ std::string sampleDataDir(const std::string& file)
 
 int main(int argc, char** argv)
 {
-    Logger::instance().add(new ConsoleChannel("debug", LInfo)); // LTrace, Level::Debug
+    Logger::instance().add(new ConsoleChannel("debug", Level::Info)); // LTrace, Level::Debug
     Logger::instance().setWriter(new AsyncLogWriter);
 
 //#if USE_SSL
