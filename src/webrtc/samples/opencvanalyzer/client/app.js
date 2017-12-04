1570292d08fdcf3081ab40eb3c96b4cbeaf0999e
lutsykigor <lutsyk.igor@gmail.com>
2017-12-04 23:26:18 +0100

    Added comments for Chrome support

--- src/webrtc/samples/opencvanalyzer/client/app.js
+++ src/webrtc/samples/opencvanalyzer/client/app.js
@@ -22,7 +22,20 @@ app.set('view engine', 'ejs');
 app.set('views', __dirname + '/');
 app.use(express.static(__dirname + '/assets'));
 
+//uncomment this for HTTPS support (Chrome)
 
+//var https = require('https');
+//var privateKey  = fs.readFileSync('ssl/symple.key', 'utf8'); // there are no local /ssl folder, but you can get it from other samples, or create self signed certs
+//var certificate = fs.readFileSync('ssl/symple.crt', 'utf8');
+//var credentials = {key: privateKey, cert: certificate};
+
+//var httpsServer = https.createServer(credentials, app);
+
+//httpsServer.listen(app.get('port'), function() {
+   //console.log('Web server listening on port ' + app.get('port'));
+//});
+
+//comment all next lines for HTTPS support (Chrome)
 app.get('/', function (req, res) {
   res.render('index', {
     port: serverPort,
